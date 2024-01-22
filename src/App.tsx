c00a9b0e9e89671258a426355335deed92e1cd16
Taylan Santos <taylanmf@gmail.com>
2024-01-22 11:03:20 -0300

    Componentes: Lista e Formulário

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 
 import Navbar from './components/navbar/NavBar';
@@ -7,6 +8,8 @@ import Login from './pages/login/Login';
 import Cadastro from './pages/cadastro/Cadastro';
 import Home from './pages/home/Home';
 import { AuthProvider } from './contexts/AuthContext';
+import ListaTemas from './components/temas/listaTemas/ListaTemas';
+import FormularioTema from './components/temas/formularioTema/FormularioTema';
 
 
 function App() {
@@ -21,6 +24,10 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+
             </Routes>
           </div>
           <Footer />
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -5,16 +5,31 @@ const api = axios.create({
 	baseURL: "https://blogpessoal-xee7.onrender.com/",
 });// temos que criar um objeto que será nocectado ao backend
 
-export const cadastrarUsuario = async (
-	url: string,
-	dados: Object,
-	setDados: Function
-) => {
-	const resposta = await api.post(url, dados);
-	setDados(resposta.data);
-};
-// função assincrona = elas esperam que uma ação finalize e só então  continua sua logica 
-export const login = async (url: string, dados: Object, setDados: Function) => {
-	const resposta = await api.post(url, dados);
-	setDados(resposta.data);
-};
+export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
+	const resposta = await api.post(url, dados)
+	setDados(resposta.data)
+  }
+  
+  export const login = async(url: string, dados: Object, setDados: Function) => {
+	const resposta = await api.post(url, dados)
+	setDados(resposta.data)
+  }
+  
+  export const buscar = async(url: string, setDados: Function, header: Object) => {
+	const resposta = await api.get(url, header)
+	setDados(resposta.data)
+  }
+  
+  export const cadastrar = async(url: string, dados: Object, setDados: Function, header: Object) => {
+	const resposta = await api.post(url, dados, header)
+	setDados(resposta.data)
+  }
+  
+  export const atualizar = async(url: string, dados: Object, setDados: Function, header: Object) => {
+	const resposta = await api.put(url, dados, header)
+	setDados(resposta.data)
+  }
+  
+  export const deletar = async(url: string, header: Object) => {
+	await api.delete(url, header)
+  }
